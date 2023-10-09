d63d55487af01af09ebc1f4b36f6f289c24d019c
Maren Christensen <marenjoychristensen@gmail.com>
2023-10-09 17:42:41 -0500

    fixed paranthesis error in .then and .catch functions for the dashboard route.

--- controllers/api/dashboard-routes.js
+++ controllers/api/dashboard-routes.js
@@ -33,10 +33,9 @@ router.get('/', withAuth, (req, res) => {
     .then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true}));
         res.render('dashboard', { posts, loggedIn: true });
-    
-}); .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-});
-}
-);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+} );

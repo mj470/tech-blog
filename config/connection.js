b5ab86451182eb113f477e182b6b7799787a37a2
Maren Christensen <marenjoychristensen@gmail.com>
2023-10-10 18:03:24 -0500

    changed mysql port from 3001 to default 3306

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
         host: '127.0.0.1',
         dialect: 'mysql',
-        port: 3001
+        port: 3306
     });
 }
 
